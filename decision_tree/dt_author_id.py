ae44e6f1872cbdb4eeea98467cb67a99987ffdbb
Yu Yang <yu.yang@cittacorp.com>
2018-01-09 10:14:57 -0500

    finish decision tree part

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,7 +24,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+print("the number of features:", len(features_train[0]))
+from sklearn import tree
+CLF = tree.DecisionTreeClassifier(min_samples_split=40)
+T0 = time()
+CLF = CLF.fit(features_train, labels_train)
+print("decision tree training time:", round(time()-T0, 3), "s")
+T1 = time()
+PRED = CLF.predict(features_test)
+print("decision tree predition time:", round(time()-T1, 3), "s")
+ACC = CLF.score(features_test, labels_test)
+print(ACC)
 
 #########################################################
 
